248da1886041a64e995b3f09e425a2f3ea893f3f
KuldeepKumarAngural <kuldeep.navv@gmail.com>
2023-07-27 16:45:05 +0530

    Simple GraphQl first EndPoint

--- Backend/App.js
+++ Backend/App.js
@@ -1,12 +1,41 @@
-const express=require('express');
-const bodyParser=require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHTTP = require('express-graphql').graphqlHTTP;
+const { buildSchema } = require('graphql');
 require('dotenv').config();
-
-const app=express();
-
-app.use(bodyParser.json());
 const port=process.env.PORT||8080;
 
-app.listen(port,()=>{
-    console.log("Server started on port : ",port);
-})
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      events: () => {
+        return ['Romantic Cooking', 'Sailing', 'All-Night Coding'];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      }
+    },
+    graphiql: true
+  })
+);
+app.listen(port, () => console.log("Server started on port : ", port));
