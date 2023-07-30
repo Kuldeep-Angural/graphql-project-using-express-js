747d15df5f8c6ac72beea8c9ae938b2ba01bed32
KuldeepKumarAngural <kuldeep.navv@gmail.com>
2023-07-30 15:44:00 +0530

    Minor changes

--- Backend/App.js
+++ Backend/App.js
@@ -20,13 +20,12 @@ app.use(
 mongoose
   .connect(process.env.MONGO_DATABASE_SRV)
   .then(() => {
-    console.log(
-      "Wait until Project Build Finishes")
+    console.log("Wait until Project Build Finishes");
     app.listen(port, () =>
       console.log(
         "Congratulations Server started Successfully on port:",
         port,
-       ", Data base name :",
+        ", Data base name :",
         process.env.MONGO_DB
       )
     );
